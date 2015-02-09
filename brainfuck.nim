d8b00895b010941c8d9449ff079d2c442ae07497
Finlay Maguire <finlaymaguire@gmail.com>
2015-02-09 18:01:03 +0000

    Implement basic brainfuck syntax apart from loops

--- brainfuck.nim
+++ brainfuck.nim
@@ -3,4 +3,24 @@ import os
 let code = if paramCount() > 0: readFile paramstr(1)
            else: readAll stdin
 
-echo code
+var
+    tape = newSeq[char]()
+    codePos = 0
+    tapePos = 0
+
+proc run(skip = false): bool = 
+    while tapePos >= 0 and codePos < code.len:
+        echo "codePos: ", codePos, " tapePos: ", tapePos
+        if tapePos >= tape.len:
+            tape.add '\0'
+        case code[codePos]
+        of '+': inc tape[tapePos]
+        of '-': dec tape[tapePos]
+        of '>': inc tapePos
+        of '<': dec tapePos
+        of '.': stdout.write tape[tapePos]
+        of ',': tape[tapePos] = stdin.readChar
+        else: discard
+        inc codePos
+
+discard run()
