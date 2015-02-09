31973f54d9b67307f6ba2863e22fae98806ead70
Finlay Maguire <finlaymaguire@gmail.com>
2015-02-09 18:02:54 +0000

    Implement brainfuck looping syntax

--- brainfuck.nim
+++ brainfuck.nim
@@ -10,17 +10,25 @@ var
 
 proc run(skip = false): bool = 
     while tapePos >= 0 and codePos < code.len:
-        echo "codePos: ", codePos, " tapePos: ", tapePos
         if tapePos >= tape.len:
             tape.add '\0'
-        case code[codePos]
-        of '+': inc tape[tapePos]
-        of '-': dec tape[tapePos]
-        of '>': inc tapePos
-        of '<': dec tapePos
-        of '.': stdout.write tape[tapePos]
-        of ',': tape[tapePos] = stdin.readChar
-        else: discard
+
+        if code[codePos] == '[':
+            inc codePos
+            let oldPos = codePos
+            while run(tape[tapePos] == '\0'):
+                codePos = oldPos
+        elif code[codePos] == ']':
+            return tape[tapePos] != '\0'
+        elif not skip:
+            case code[codePos]
+            of '+': inc tape[tapePos]
+            of '-': dec tape[tapePos]
+            of '>': inc tapePos
+            of '<': dec tapePos
+            of '.': stdout.write tape[tapePos]
+            of ',': tape[tapePos] = stdin.readChar
+            else: discard
         inc codePos
 
 discard run()
